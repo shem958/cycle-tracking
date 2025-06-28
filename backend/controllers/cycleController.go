4317c6c3d730b07f4f64c9aab5fe00f6077f62d6
shem958 <shemgikunda37@gmail.com>
2025-06-28 22:16:45 +0300

    Add cycleController with GetCycles and AddCycle functions

---
+++ backend/controllers/cycleController.go
@@ -0,0 +1,24 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"github.com/shem958/cycle-backend/models"
+	"github.com/shem958/cycle-backend/config"
+)
+
+func GetCycles(c *gin.Context) {
+	var cycles []models.Cycle
+	config.DB.Find(&cycles)
+	c.JSON(http.StatusOK, cycles)
+}
+
+func AddCycle(c *gin.Context) {
+	var cycle models.Cycle
+	if err := c.ShouldBindJSON(&cycle); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	config.DB.Create(&cycle)
+	c.JSON(http.StatusOK, cycle)
+}
