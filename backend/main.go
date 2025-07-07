e9cfa8243c1a9b65f74f26c490519638fd6cda3d
shem958 <shemgikunda37@gmail.com>
2025-07-07 15:49:12 +0300

    Enhance CORS configuration: add Authorization header to AllowHeaders for improved API security

--- backend/main.go
+++ backend/main.go
@@ -13,7 +13,8 @@ func main() {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+
 		AllowCredentials: true,
 	}))
 
